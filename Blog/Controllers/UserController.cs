fcccd347c46550ab353cba021e3a21652d6b91c5
chillnchill <109694910+chillnchill@users.noreply.github.com>
2024-05-18 21:40:05 +0300

    Added very basic User controller

---
+++ Blog/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Blog.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class UserController : Controller
+    {
+        private SignInManager<IdentityUser> signInManager;
+
+        public UserController(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel vm)
+        {
+            var result = await signInManager.PasswordSignInAsync(vm.UserName, vm.Password, false, false);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Blog/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}

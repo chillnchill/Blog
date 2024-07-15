cdd037fea770826a05d0b86f968bed3a9ba9c6e5
chillnchill <jojo.chillz@gmail.com>
2024-07-15 15:17:45 +0300

    comments view

--- obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29fa85acc6b686c009b47451f49aa2a6e1e68ef8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026989114a9775edcf8e19ab72d943c95ffacddb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

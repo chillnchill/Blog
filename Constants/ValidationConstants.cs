6ca418f42ea371c7f00b6d666f693b4783123bb5
chillnchill <109694910+chillnchill@users.noreply.github.com>
2024-09-02 19:03:49 +0300

    Added validation constants to the CommentViewModel

---
+++ Constants/ValidationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Constants
+{
+	public class ValidationConstants
+	{
+		public const int CommentsMaxLength = 15360;
+		public const int CommentsMinLength = 3;
+		public const string CommentsError = "Comments need to be at least 3 characters long";
+	}
+}

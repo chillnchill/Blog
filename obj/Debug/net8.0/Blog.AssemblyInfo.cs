dc758c3aefcd8fab1015cc9eede50d701636a921
chillnchill <109694910+chillnchill@users.noreply.github.com>
2024-09-04 00:49:25 +0300

    Changed Image field in Post entity to nullable

--- obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ca418f42ea371c7f00b6d666f693b4783123bb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001ec729828b2a2213061161de2f5bc0d410f690")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

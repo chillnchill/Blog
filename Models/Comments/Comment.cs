d35363949287848fd88b49282dbf0bfcae9fd299
chillnchill <109694910+chillnchill@users.noreply.github.com>
2024-09-06 22:57:21 +0300

    Added editing for user's comments

--- Models/Comments/Comment.cs
+++ Models/Comments/Comment.cs
@@ -8,9 +8,11 @@ namespace Blog.Models.Comments
         public int Id { get; set; }
 
 		[Required]
+		public string UserId { get; set; } = null!;
+
+		[Required]
 		public string Message { get; set; } = string.Empty;
 
         public DateTime CreatedOn { get; set; }
-
     }
 }
--- obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001ec729828b2a2213061161de2f5bc0d410f690")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc758c3aefcd8fab1015cc9eede50d701636a921")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
